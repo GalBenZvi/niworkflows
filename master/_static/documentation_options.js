092fe703e3eb5928f1ab572bce1cdb15501e9728
Oscar Esteban <code@oscaresteban.es>
2021-10-14 09:45:36 +0200

    enh: update development version of docs

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.1+8.g3ff4869a10',
+    VERSION: '1.4.1+8.g3ff4869a10.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
